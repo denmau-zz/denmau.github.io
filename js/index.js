45fc111c84a1f425d3ee3c0cc91e901a06f12737
Dennis Kamau <dmkamau475@gmail.com>
2021-01-15 19:03:31 +0300

    update index.html

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
-// Collect all the inputs
-const inputs = document.querySelectorAll("input, select, textarea");
 
-// Adds an invalid class to an invalid input
-// This input is then styled by CSS to show invalid input
-inputs.forEach((input) => {
-  input.addEventListener(
-    "invalid",
-    (event) => {
-      input.classList.add("invalid");
-    },
-    false
-  );
-});
