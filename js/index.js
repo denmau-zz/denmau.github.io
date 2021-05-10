b227f148f4eb5d2ea2e193dae3bc88632dce9fcb
Denmau <dmkamau475@gmail.com>
2021-05-10 16:46:34 +0300

    initial deployment

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
+alert("Hello, the development of this site is in progress. It is currently optimised for mobile. Thank you :-)");
+
 // Collect all the inputs
 const inputs = document.querySelectorAll("input, select, textarea");
 
 // Adds an invalid class to an invalid input
 // This input is then styled by CSS to show invalid input
 inputs.forEach((input) => {
-  input.addEventListener(
-    "invalid",
-    () => {
-      input.classList.add("invalid");
-    },
-    false
-  );
+    input.addEventListener(
+        "invalid",
+        () => {
+            input.classList.add("invalid");
+        },
+        false
+    );
 });
-
-// function alertCVNotAvailable() {
-//   alert("Hello, I am in the process of editing my CV, it should be up in a few days. Thank you.");
-// }
