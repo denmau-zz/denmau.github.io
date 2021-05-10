a234a4ebba7811ad10cfbc2d6998f1b7f654e6ce
Restyled.io <commits@restyled.io>
2021-05-10 14:02:27 +0000

    Restyled by prettier

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 alert(
-    "Hello, the development of this site is in progress. It is currently optimised for mobile. Thank you :-)");
+  "Hello, the development of this site is in progress. It is currently optimised for mobile. Thank you :-)"
+);
 
 // Collect all the inputs
 const inputs = document.querySelectorAll("input, select, textarea");
@@ -7,6 +8,11 @@ const inputs = document.querySelectorAll("input, select, textarea");
 // Adds an invalid class to an invalid input
 // This input is then styled by CSS to show invalid input
 inputs.forEach((input) => {
-  input.addEventListener("invalid", () => { input.classList.add("invalid"); },
-                         false);
+  input.addEventListener(
+    "invalid",
+    () => {
+      input.classList.add("invalid");
+    },
+    false
+  );
 });
