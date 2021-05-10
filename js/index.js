ca66a4f6b1964ad705a588250803263a5e22bacc
Restyled.io <commits@restyled.io>
2021-05-10 14:02:19 +0000

    Restyled by clang-format

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 alert(
-  "Hello, the development of this site is in progress. It is currently optimised for mobile. Thank you :-)"
-);
+    "Hello, the development of this site is in progress. It is currently optimised for mobile. Thank you :-)");
 
 // Collect all the inputs
 const inputs = document.querySelectorAll("input, select, textarea");
@@ -8,11 +7,6 @@ const inputs = document.querySelectorAll("input, select, textarea");
 // Adds an invalid class to an invalid input
 // This input is then styled by CSS to show invalid input
 inputs.forEach((input) => {
-  input.addEventListener(
-    "invalid",
-    () => {
-      input.classList.add("invalid");
-    },
-    false
-  );
+  input.addEventListener("invalid", () => { input.classList.add("invalid"); },
+                         false);
 });
