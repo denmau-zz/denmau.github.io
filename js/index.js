bc7966ce619943b27a3cf747e9b3e2a9d2e101b9
Denmau <dmkamau475@gmail.com>
2021-05-12 08:28:59 +0300

    update alert message

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// alert("Hello, the development of this site is in progress. It is currently optimised for mobile. Thank you :-)");
+alert("Hello, I am Dennis Kamau, I am still developing this site. Feel free to explore. Thank you.");
 
 // Collect all the inputs
 const inputs = document.querySelectorAll("input, select, textarea");
@@ -6,11 +6,11 @@ const inputs = document.querySelectorAll("input, select, textarea");
 // Adds an invalid class to an invalid input
 // This input is then styled by CSS to show invalid input
 inputs.forEach((input) => {
-  input.addEventListener(
-    "invalid",
-    () => {
-      input.classList.add("invalid");
-    },
-    false
-  );
+    input.addEventListener(
+        "invalid",
+        () => {
+            input.classList.add("invalid");
+        },
+        false
+    );
 });
