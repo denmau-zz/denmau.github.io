8981212c7bdb850c1af5c83eaaedc45907631dab
Dennis Kamau <73742235+denmau@users.noreply.github.com>
2021-08-07 17:40:17 +0300

    add foundation for recaptcha

--- js/index.js
+++ js/index.js
@@ -1,17 +1,35 @@
-// alert("Hello, I am Dennis Kamau, I am still developing this site. Feel free
-// to explore. Thank you.");
+$(document).ready(function() {
+  function onSubmit(token) {
+    document.getElementById("contact-form").submit();
+  }
+  $("#contact-form").submit(function() {
+    grecaptcha.ready(() => {
+      grecaptcha
+        .execute("6LdkN-YbAAAAACqFnhzWnvWH5BIxbrSG1eoza7PJ", {
+          action: "submit",
+        })
+        .then((token) => {
+          // do a bunch of verification steps here
+          // TODO will implement this later
+          // as soon as I hoop my static site to a server
+        });
+    });
 
-// Collect all the inputs
-const inputs = document.querySelectorAll("input, select, textarea");
+    return true; // return false to cancel form action
+  });
 
-// Adds an invalid class to an invalid input
-// This input is then styled by CSS to show invalid input
-inputs.forEach((input) => {
-  input.addEventListener(
-    "invalid",
-    () => {
-      input.classList.add("invalid");
-    },
-    false
-  );
+  // Collect all the inputs
+  const inputs = document.querySelectorAll("input, select, textarea");
+
+  // Adds an invalid class to an invalid input
+  // This input is then styled by CSS to show invalid input
+  inputs.forEach((input) => {
+    input.addEventListener(
+      "invalid",
+      () => {
+        input.classList.add("invalid");
+      },
+      false
+    );
+  });
 });
